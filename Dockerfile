9f0c6f9e01238063cece78f17ce0e5d9da941422
a520ass <a520ass@hotmail.com>
2019-04-17 20:10:43 +0800

    修改为yum安装方式

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,18 @@ FROM centos:centos7
 
 LABEL name="CentOS7 with JDK8"
 
-ENV JAVA_VERSION 8
-ENV JAVA_FOLDER_NAME jdk1.8.0_202
-ENV JDK_HOME /usr/java/${JAVA_FOLDER_NAME}
-ENV JAVA_HOME /usr/java/${JAVA_FOLDER_NAME}/jre
+#修改时间和时区 https://blog.csdn.net/alinyua/article/details/80944543
+RUN ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+RUN echo 'Asia/Shanghai' >/etc/timezone
 
 COPY java-installer.sh /
 RUN /java-installer.sh
 RUN rm /java-installer.sh
+
+ENV JAVA_FOLDER_NAME jdk1.8.0_211-amd64
+ENV JAVA_HOME /usr/java/${JAVA_FOLDER_NAME}
+ENV JRE_HOME /usr/java/${JAVA_FOLDER_NAME}/jre
+ENV PATH=$JAVA_HOME/bin:$PATH \
+    CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tool.jar:$JAVA_HOME/lib
 
 CMD ["/bin/bash"]
--- java-installer.sh
+++ java-installer.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
-JAVA_URL="https://download.oracle.com/otn-pub/java/jdk/8u202-b08/1961070e4c9b4e26a04e7f5a083f551e/jdk-8u202-linux-x64.tar.gz"
-# Checksum via https://www.oracle.com/webfolder/s/digest/8u172checksum.html
-CHECKSUM="9a5c32411a6a06e22b69c495b7975034409fa1652d03aeb8eb5b6f59fd4594e0"
+JAVA_URL="http://hffile.oss-cn-hangzhou.aliyuncs.com/jdk-8u211-linux-x64.rpm"
+# Checksum via https://www.oracle.com/webfolder/s/digest/8u211checksum.html
+CHECKSUM="6e0274e95edb94e1cabf091c7c6035e22256080f746ba605958c961a5fd032fb"
 
 curl \
 	-L \
-	-H "Cookie: oraclelicense=accept-securebackup-cookie" \
-	-o /tmp/jdk.tar.gz \
+	-H "Connection: keep-alive" \
+	-o /tmp/jdk.rpm \
 	"${JAVA_URL}"
 
-DOWNLOADED_CHECKSUM=`sha256sum /tmp/jdk.tar.gz | grep -o -E -e "[a-f0-9]*\s" | tr -d '[[:space:]]'`
+DOWNLOADED_CHECKSUM=`sha256sum /tmp/jdk.rpm | grep -o -E -e "[a-f0-9]*\s" | tr -d '[[:space:]]'`
 
 echo "Checksum from Oracle is: ${CHECKSUM}"
 echo "Downloaded checksum is: ${DOWNLOADED_CHECKSUM}"
 
 if [ "${CHECKSUM}" != "${DOWNLOADED_CHECKSUM}" ]; then
 	echo "Checksum verification failed; install aborted."
-	rm /tmp/jdk.tar.gz
+	rm /tmp/jdk.rpm
 	exit 1
 fi
 
-mkdir -p /usr/java
-tar -xzf /tmp/jdk.tar.gz -C /usr/java
-chown -R root:root ${JDK_HOME}
-ln -s ${JDK_HOME} /usr/java/jdk1.8
-echo "PATH=$PATH:/usr/java/jdk1.8/jre/bin:/usr/java/jdk1.8/bin" > /etc/profile.d/java
-rm /tmp/jdk.tar.gz
+yum install -y /tmp/jdk.rpm
+rm /tmp/jdk.rpm
 
